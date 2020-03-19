d73fd89a4d7cb8403539d1d618e0af094fe14ca6
Mirko Nasato <mirko@uptoskill.com>
2020-03-19 11:48:29 +0000

    Style BiorhythmCard result colours

--- src/components/BiorhythmCard.js
+++ src/components/BiorhythmCard.js
@@ -8,6 +8,7 @@ import dayjs from 'dayjs';
 import React from 'react';
 import { calculateBiorhythms } from '../calculations';
 import BiorhythmChart from './BiorhythmChart';
+import './BiorhythmCard.css';
 
 function formatDate(isoString) {
   return dayjs(isoString).format('D MMM YYYY');
@@ -17,15 +18,15 @@ function BiorhythmCard({ birthDate, targetDate }) {
   const { physical, emotional, intellectual } =
     calculateBiorhythms(birthDate, targetDate);
   return (
-    <IonCard className="ion-text-center">
+    <IonCard className="biorhythm-card ion-text-center">
       <IonCardHeader>
         <IonCardTitle>{formatDate(targetDate)}</IonCardTitle>
       </IonCardHeader>
       <IonCardContent>
         <BiorhythmChart birthDate={birthDate} targetDate={targetDate} />
-        <p>Physical: {physical.toFixed(4)}</p>
-        <p>Emotional: {emotional.toFixed(4)}</p>
-        <p>Intellectual: {intellectual.toFixed(4)}</p>
+        <p className="physical">Physical: {physical.toFixed(4)}</p>
+        <p className="emotional">Emotional: {emotional.toFixed(4)}</p>
+        <p className="intellectual">Intellectual: {intellectual.toFixed(4)}</p>
       </IonCardContent>
     </IonCard>
   );
