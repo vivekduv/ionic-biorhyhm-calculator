919eb070d0a7ae6c17de81f8bef903acf3073220
Mirko Nasato <mirko@uptoskill.com>
2020-03-19 11:25:33 +0000

    Store birthDate in localStorage using a custom hook

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import {
 } from '@ionic/react';
 import React, { useState } from 'react';
 import BiorhythmCard from './components/BiorhythmCard';
+import { useLocalStorage } from './hooks';
 
 function App() {
-  const [birthDate, setBirthDate] = useState('');
+  const [birthDate, setBirthDate] = useLocalStorage('birthDate', '');
   const [targetDate, setTargetDate] = useState(new Date().toISOString());
   return (
     <IonApp>
