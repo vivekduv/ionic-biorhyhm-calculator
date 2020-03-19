e21310f0f2643102f541ad6e3f80f4704bae0e79
Mirko Nasato <mirko@uptoskill.com>
2020-03-19 11:15:01 +0000

    Calculate the biorhythms, with unit tests

---
+++ src/calculations.test.js
@@ -0,0 +1,16 @@
+import { calculateBiorhythms } from './calculations';
+
+it('calculates the physical biorhythm', () => {
+  const { physical } = calculateBiorhythms('1995-01-01', '2020-02-18');
+  expect(physical).toBeCloseTo(0.5196);
+});
+
+it('calculates the emotional biorhythm', () => {
+  const { emotional } = calculateBiorhythms('1995-01-01', '2020-02-18');
+  expect(emotional).toBeCloseTo(-0.9010);
+});
+
+it('calculates the intellectual biorhythm', () => {
+  const { intellectual } = calculateBiorhythms('1995-01-01', '2020-02-18');
+  expect(intellectual).toBeCloseTo(0.8146);
+});
